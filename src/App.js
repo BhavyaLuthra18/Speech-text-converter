2d3afd241f4caff9f606c0eccd6ff6de4db54152
Bhavya Luthra <bhavyaluthra1234@gmail.com>
2024-02-01 18:47:27 +0530

    Speech-to-text-converter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
-function App() {
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import useClipboard from "react-use-clipboard";
+
+const App = () => {
+  const [textToCopy, setTextToCopy] = useState();
+  const [isCopied, setCopied] = useClipboard(textToCopy, {
+    successDuration: 2000,
+  });
+
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+
+  const { transcript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+
+  const handleCopyToClipboard = () => {
+    // checking if the text is available in transcript if yes then copy the text to the clipboard
+    if (transcript) {
+      navigator.clipboard
+        .writeText(transcript) // to write text on clipboard
+        .then(() => {
+          setCopied(true);
+
+          setTimeout(() => {
+            setCopied(false);
+          }, 1000);
+        })
+        .catch((error) => {
+          console.error("Failed to copy:", error);
+        });
+    }
+  };
+
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+  
+        <h2>
+        <img src="images/mic.png"/>
+          Speech to Text Converter</h2>
+     
+        <br />
+        <p>With speech to text, a user can convert any audio note into textual content quickly.</p>
+        <div className="main-content" onClick={() => setTextToCopy(transcript)}>
+          {transcript}
+        </div>
+        <div className="btn-style">
+          <button onClick={handleCopyToClipboard}>
+            {isCopied ? "Copied!" : "Copy to clipboard"}
+          </button>
+
+          <button onClick={startListening}>Start Listening</button>
+          <button onClick={SpeechRecognition.stopListening}>
+            Stop Listening
+          </button>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
